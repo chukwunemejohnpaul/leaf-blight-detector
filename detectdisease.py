8b267aaa44d8e4351bc3e813f8a5cac86d40ba3f
Tadinma Chukwuneme <officiallychuks@gmail.com>
2019-09-03 13:21:39 -0700

    edited the detectdisease.py file

--- detectdisease.py
+++ detectdisease.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-img = cv2.imread('ricedisease5.jpeg')
+img = cv2.imread('ricedisease6.jpg')
 blurred = cv2.GaussianBlur(img,(5,5),-1)
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -28,18 +28,18 @@ _,contours, _ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
 
 
-ggg = cv2.drawContours(img,contours,-1,(0,0,255),2)
+image = cv2.drawContours(img,contours,-1,(0,0,255),2)
 
-if contours:
-	cv2.putText(img,"rice bleat detected", (30, 50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-"""for cnt in contours:
+"""if contours:
+	cv2.putText(img,"rice bleat detected", (30, 50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)"""
+for cnt in contours:
 	(x,y,w,h) = cv2.boundingRect(cnt)
-	cv2.rectangle(img,(x,y),(x+w,y+h),(200,45,67),2)"""
+	cv2.rectangle(image,(x,y),(x+w,y+h),(200,45,67),2)
 
 #ggg = cv2.drawContours(ggg,contours,-1,(11,222,13),3)
 
 
 cv2.imshow("pixels",green)
-cv2.imshow("pix",ggg)
+cv2.imshow("pix",image)
 k = cv2.waitKey(0)
 cv2.destroyAllWindows()
